ef58a2d968f8e7f3e6201a42b33fb085cca51889
Mohamed Elsherif <bashmohandes@live.com>
2016-12-21 20:16:25 -0800

    One more question

---
+++ leetcode/searchforrange.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function(nums, target) {
+    var start = 0
+    var end = nums.length - 1
+    var med = Math.floor(end / 2)
+    var startIndex = 0
+    var endIndex = 0
+    while(med >= start && med <= end) {                
+        if(nums[med] === target) {
+            // find start and end indices
+            startIndex = med
+            endIndex = med
+            while(startIndex > 0 && nums[startIndex - 1] === nums[med]) {
+                startIndex --
+            }
+            while(endIndex < nums.length - 1 && nums[endIndex + 1] === nums[med]) {
+                endIndex ++
+            }
+            
+            return [startIndex, endIndex]
+        } else if (target < nums[med]) {
+            end = med - 1
+        } else {
+            start = med + 1
+        }        
+        med = start + Math.floor((end - start) / 2)
+    }
+    
+    return [-1, -1]
+};
+
+
+console.log(searchRange([5, 7, 7, 8, 8, 10], 8))
