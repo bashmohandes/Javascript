c7f6f5e33d8c4942d1eb4bd08b8a8d20f56c0cc6
Mohamed Elsherif <bashmohandes@live.com>
2017-01-16 13:44:03 -0800

    added difficulty slider

--- Sudoku/scripts/sketch.js
+++ Sudoku/scripts/sketch.js
@@ -2,13 +2,16 @@
 var board
 var CELL_SIZE = 80
 var DIFFICULTY = 0.4
-
+var slider 
 function setup() {    
     var canvas = createCanvas(CELL_SIZE * 9, CELL_SIZE * 9)
     canvas.parent("canvas")
     board = new Board()
-    board.build()
-    var btn = select("#btnGenerate")
+    board.build()    
+
+    createSpan("Difficulty")
+    slider = createSlider(0.1, 0.7, 0.4, 0.1)
+    var btn = createButton("Generate")
     btn.mousePressed(function(){
         board.clear()
         board.build()
@@ -18,6 +21,7 @@ function setup() {
 
 function draw() {
     background(255)    
+    DIFFICULTY = slider.value()
     board.draw()
 }
 
